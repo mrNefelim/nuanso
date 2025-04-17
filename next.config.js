0b1aa029a196f14149dc23e0164113f956518e3a
Дмитрий Сорокин <mr.nefelim@yandex.ru>
2025-04-17 22:47:46 +0300

    Fix build and deployment configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/blind_spot',
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig; 
