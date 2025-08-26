19f0f0aea114ef3bc50d9c5fb726219bc397288e
Дмитрий Сорокин <mr.nefelim@yandex.ru>
2025-08-26 16:39:05 +0300

    Use project as root

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  basePath: '/',
+  basePath: '/nuanso',
   assetPrefix: '/',
   images: {
     unoptimized: true
